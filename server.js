9dc2c5e62ee22aa1e9754ba93dca6958260e4c02
Stella <chiara.poma@mail.polimi.it>
2021-11-27 15:31:22 +0100

    added socket

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ let server = app.listen(port); // the connection will happen in the port 3000
 console.log("server is running on http://localhost:" + port);
 
 app.use(express.static("public")); //the file in this folder will be static, so don't change when server is runninng
+
+let serverSocket = require("socket.io"); //serveresocket is the name of the variable
+
+let io = serverSocket(server); // input output variable
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
